e0e409dabaf8d72ad102d60c52b4905102d0769c
lvaillan <classvaillan@gmail.com>
2021-09-04 14:39:41 -0500

    more updates to the routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,21 @@
-const router = require('express').Router();
+const notesData = require('../db/db.json');
 
-const noteData = require("../db/db.json");
-
-module.exports = function(app) {
-
-    app.get("api/notes", function (req, res) {
-        res.json(noteData)
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(notesData)
     });
 
-}
+    // var newStore = {
+    //     storeID: $("#storeID").val().trim(),
+    //     storeName: $("#storeName").val().trim(),
+    //     storeDescription: $("#storeDescription").val().trim(),
+    //     status: $("#status").val().trim()
+    // };
+
+    app.post("/api/notes", function (req, res) {
+        console.log(req);
+        notesData.push(req.body);
+        res.json(true);
+    });
+
+};
